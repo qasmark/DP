8b7ff9aaa5a79c306915cb0dd2082bf2307158ea
qasmark <99826972+qasmark@users.noreply.github.com>
2024-03-11 22:44:39 +0300

    Delete pa1 directory

--- pa1/Valuator/Config/Configs.cs
+++
@@ -1,9 +0,0 @@
-using Microsoft.AspNetCore.Mvc;
-
-namespace Valuator.Config;
-
-public static class Configs
-{
-    public static string REDIS_HOSTNAME = "localhost";
-    public static int REDIS_PORT = 6379;
-}
--- pa1/Valuator/Config/IRedisStorage.cs
+++
@@ -1,8 +0,0 @@
-namespace Valuator.Config;
-
-public interface IRedisStorage
-{
-    void Store(string key, string value);
-    string Load(string key);
-    List<string> GetKeys();
-}
